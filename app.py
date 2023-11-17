fcdb3f33213a2cbcbbcbdc5795db8ea7d0e31783
Manuel Hettich <17690367+ManuelHettich@users.noreply.github.com>
2023-11-18 00:07:23 +0100

    Add simple OpenAI API call

--- app.py
+++ app.py
@@ -10,12 +10,14 @@ def hello_world():
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-            {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Just say 'Hello, World'."},
         ]
     )
 
-    return completion.choices[0].message
+    print(completion.choices)
+
+    return str(completion.choices[0].message.content)
 
 if __name__ == '__main__':
     app.run(debug=True)
