e666363567e55a46d66735c947f4180aa9bf0210
Manuel Hettich <17690367+ManuelHettich@users.noreply.github.com>
2023-11-17 23:45:30 +0100

    Add a simple ChatGPT prompt

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask
+from openai import OpenAI
+client = OpenAI()
+
+
 app = Flask(__name__)
 
 @app.route('/', methods=["POST"])
 def hello_world():
-    return 'Hello, World!'
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+            {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+        ]
+    )
+
+    return completion.choices
 
 if __name__ == '__main__':
     app.run(debug=True)
